8e5a3402658001cfcca623b282934d01dc2d9827
ftornay <ftornay@ugr.es>
2016-11-24 11:18:17 +0100

    Changed some comments, so the algorithm's easier to follow

--- int_int.py
+++ int_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-from __future__ import division
 def steps(min, max, n):
     """ Function that interpolates values of a variable
-    linearly between two values (min, max) in a number of steps (n)
+    linearly between (min, max) in a number of steps (n)
     it uses the "integer line algorithm", simpler than Bresenham's
     The algorithm was taken from: http://www.pyaray.com/articles/lines.htm
-    This version supposes that
-    the to-be-interpolated variable (y) varies _more_than the number of steps """
+    This version assumes that
+    the to-be-interpolated variable (y)
+    varies _more_than the number of steps (x)"""
 
     deltay = max - min
     deltax = n - 1
@@ -16,11 +16,12 @@ def steps(min, max, n):
     results = [min] # list to accumulate the values
     x = min
     for y in range(min, max+1):
-        stepVal += deltax
-        if stepVal >= deltay: # Have we incremented x more than deltay?
+        stepVal += deltax   # Everytime we _don't_ increase x
+                            # we incur in an error proportional to deltax
+        if stepVal >= deltay: # Is our error larger than deltay?
             x += 1 # Then move to the next step
-            stepVal -= deltay # we've accounted for one variation in y
-            results.append(y+1) # at the next iteration,
+            stepVal -= deltay # By increasing x, we lower our error
+            results.append(y+1) # At the next iteration,
                                 # we'll be at a new step, save the level for it
     return results
 
